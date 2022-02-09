6330008e90491146e7e34aa5174388658cdebdca
Andrey <Movlyanandrew1993@mail.ru>
2022-02-10 01:38:13 +0300

    дз 7

--- src/Lesson7HW/HomeWorkApp7.java
+++ src/Lesson7HW/HomeWorkApp7.java
@@ -1,2 +1,38 @@
-package Lesson7HW;public class HomeWorkApp7 {
-}
+package Lesson7HW;
+
+import java.util.Scanner;
+
+public class HomeWorkApp7 extends Object {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args){
+        String stringTest = "Test";
+        StringBuilder stringBuilder = new StringBuilder("Test");
+
+        long start = System.nanoTime();
+
+        for (int i = 0; i < 60000; i++) {
+            stringTest += i;
+        }
+
+        float finish = System.nanoTime() - start;
+
+        System.out.println(stringTest);
+        System.out.println("Test time string = " + finish * 0.000001f + " millisec");
+
+
+        start = System.nanoTime();
+
+        for (int i = 0; i < 60000; i++) {
+            stringBuilder.append(i); 
+        }
+
+        finish = System.nanoTime() - start;
+
+        System.out.println(stringBuilder.toString());
+        System.out.println("Test time stringBuilder = " + finish * 0.000001f + " millisec");
+        System.out.println(stringTest.equals(stringBuilder.toString()));
+
+    }
+    }
